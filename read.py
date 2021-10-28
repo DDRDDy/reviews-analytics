98056f493a5ebb83321c07e44d42ad220fc44d70
DDRDDy <emily901121@gmail.com>
2021-10-28 16:35:32 +0800

    calc avg of reviews

--- read.py
+++ read.py
@@ -1,7 +1,13 @@
-data = []
+reviews = [] #留言清單
+count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
-		data.append(line)
-		print(len(data))
+		reviews.append(line)
 
+sum_lenth = 0
+for review in reviews:
+	sum_lenth = sum_lenth + len(review) 
+	#sum_lenth += len(review)
 
+average = sum_lenth / len(reviews) #留言平均長度
+print('留言平均長度為', average)
